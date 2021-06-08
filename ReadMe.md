d7aad1633081b3e64ed52dc236e6d9222b3f444a
Daniel Guaycha <danielguaycha@gmail.com>
2021-06-08 11:24:07 -0500

    Initial Commit

---
+++ ReadMe.md
@@ -0,0 +1,20 @@
+# TodoLegal Service
+## Requirements
+Check `requirements.txt`, this service is build with python 3.9.5
+
+## libs
+```bash
+pip install Django==3.2.4
+pip install django-rest-knox==4.1.0
+pip install djangorestframework==3.12.4
+pip install django-cors-headers==3.7.0
+```
+## Migrate
+```bash
+py manage.py migrate
+```
+
+## Run
+```bash
+py manage.py runserver
+```
---
+++ api/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+# User Serializer
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'email')
+
+
+# Register Serializer
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'first_name', 'last_name', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(username=validated_data['username'],
+                                        password=validated_data['password'],
+                                        email=validated_data['email'],
+                                        first_name=validated_data['first_name'],
+                                        last_name=validated_data['last_name'])
+
+        return user
